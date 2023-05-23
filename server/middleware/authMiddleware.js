e2a8c7456c9ceaa14559d8e2af2dc810641eb504
rishi <rishi.patel@dignizant.com>
2023-05-23 14:08:57 +0530

    Update authMiddleware.js

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -13,6 +13,10 @@ const protect = asyncHandler(async (req, res, next) => {
       token = req.headers.authorization.split(" ")[1]
       const decoded = jwt.verify(token, process.env.JWT_SECRET)
       req.user = await User.findById(decoded.id).select(["-password", "-otp"])
+      if (!req.user) {
+        res.status(401)
+        throw new Error("Invalid Token")
+      }
       if (req.user && req.user.isBlocked) {
         res.status(401)
         createErrorResponse(
